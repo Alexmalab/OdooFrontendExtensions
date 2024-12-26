6f9697746c75e36a1ab2c295de1e51b2142d578e
Alex <alexandrmarr@gmail.com>
2024-12-26 11:03:49 +0800

    add image viewer widget

---
+++ image_viewer/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2024-2025 Alexandre Ma <alex.ma@hatchtec.com> - HatchTec IT dept.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    'name': 'Image Viewer',
+    'version': '17.0.1.0',
+    'summary': 'Image Viewer Using Odoo File Viewer',
+    'category': 'Widgets',
+    'author': 'Alexandre Marr',
+    'website': 'https://github.com/Alexmalab',
+    'depends': ['base','base_setup','web'],
+    'assets': {
+        'web.assets_backend': [
+            'image_viewer/static/src/components/**/*',
+        ],
+    },
+    'license': 'AGPL-3',
+    'installable': True,
+    'application': False,
+}
---
+++ image_viewer/static/src/components/image_viewer/image_viewer_field.js
@@ -0,0 +1,30 @@
+/** @odoo-module **/
+
+import { registry } from "@web/core/registry";
+import { useFileViewer } from "@web/core/file_viewer/file_viewer_hook";
+import { ImageField, imageField } from "@web/views/fields/image/image_field";
+
+class ImagePreviewField extends ImageField {
+    static template="image_viewer.ImagePreviewField"
+    setup() {
+        debugger
+        super.setup();
+        this.fileViewer = useFileViewer();
+    }
+
+    openPreview() {
+        const imageFile = {
+            defaultSource: this.getUrl(this.props.name),  // 图片的 base64 或 URL
+            isImage: true,
+            isViewable: true,
+            displayName: this.props.name || "Image Preview"
+        };
+        this.fileViewer.open(imageFile);
+    }
+}
+
+
+registry.category("fields").add("image_viewer", {
+    ...imageField,
+    component: ImagePreviewField,
+});
