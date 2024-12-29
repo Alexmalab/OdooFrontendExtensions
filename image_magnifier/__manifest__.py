e72ce4eddd0edcae1a46db9efa9fe0f2e76e2394
Alex <alexandrmarr@gmail.com>
2024-12-29 12:31:52 +0800

    image_magnifier: update manifest

--- image_magnifier/__manifest__.py
+++ image_magnifier/__manifest__.py
@@ -1,11 +1,12 @@
 # Author: Alexandre Ma <a1exma@hotmail.com>
 # Copyright 2024 Alexandre Ma <alex.ma@hatchtec.com> - HatchTec IT Dept.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# Reference https://www.codehim.com/zoom/zoom-image-on-mouseover-using-javascript/
 
 {
     'name': 'Image Magnifier Widget',
     'version': '17.0.1.0',
-    'summary': 'Image Zoomer Widget',
+    'summary': 'Add `image_magnifier` field widget',
     'category': 'Widgets',
     'author': 'Alexandre Ma',
     'website': 'https://github.com/Alexmalab',
