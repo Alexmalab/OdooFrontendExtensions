874e14aff9d1519b4ef8cdefc9b7381347fdc140
Alex <alexandrmarr@gmail.com>
2025-08-15 15:34:41 +0800

    many2one_country_flag: add readme

--- many2one_country_flag/__manifest__.py
+++ many2one_country_flag/__manifest__.py
@@ -1,15 +1,15 @@
 # Author: Alexandre Ma <a1exma@hotmail.com>
-# Copyright 2024 Alexandre Ma <alex.ma@hatchtec.com> - HatchTec IT Dept.
+# Copyright 2024 Alexandre Ma <a1exma@hotmail.com>.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'Country Flag Widget',
     'version': '18.0.1.0',
-    'summary': 'Country Flag Widget',
+    'summary': 'Country flag field widget for form, list and kanban views',
     'category': 'Alex Frontend',
     'author': 'Alexandre Ma',
-    'website': 'https://github.com/Alexmalab',
-    'depends': ['base','base_setup','web','alex_frontend_base'],
+    'website': 'https://github.com/Alexmalab/OdooFrontendExtensions/tree/18.0/many2one_country_flag',
+    'depends': ['base','base_setup','web'],
     'assets': {
         'web.assets_backend': [
             'many2one_country_flag/static/src/components/many2one_country_flag_field/*.js',
